69592ae256c0894248198725351a4239c766a61b
Lars Berscheid <lars.berscheid@kit.edu>
2021-07-16 14:37:14 +0200

    use system cmake

--- .github/workflows/install-base.sh
+++ .github/workflows/install-base.sh
@@ -1,19 +1,19 @@
-# Install OpenCV
-git clone https://github.com/opencv/opencv.git
-cd opencv
-git checkout 4.5.2
-mkdir build && cd build
-cmake -DWITH_VTK=OFF -DWITH_GTK=OFF -DWITH_PROTOBUF=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DWITH_WEBP=OFF ..
-make -j2
-make install
-cd ../../
+# # Install OpenCV
+# git clone https://github.com/opencv/opencv.git
+# cd opencv
+# git checkout 4.5.2
+# mkdir build && cd build
+# cmake -DWITH_VTK=OFF -DWITH_GTK=OFF -DWITH_PROTOBUF=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DWITH_WEBP=OFF ..
+# make -j2
+# make install
+# cd ../../
 
-# Install PyBind11
-git clone https://github.com/pybind/pybind11.git
-cd pybind11
-git checkout v2.6.2
-mkdir build && cd build
-cmake -DPYBIND11_TEST=OFF ..
-make -j2
-make install
-cd ../../
+# # Install PyBind11
+# git clone https://github.com/pybind/pybind11.git
+# cd pybind11
+# git checkout v2.6.2
+# mkdir build && cd build
+# cmake -DPYBIND11_TEST=OFF ..
+# make -j2
+# make install
+# cd ../../
