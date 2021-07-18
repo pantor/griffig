3fb11031fdb032351789953cef992aea6b252fb2
Lars Berscheid <lars.berscheid@kit.edu>
2021-07-18 20:53:47 +0200

    ls /usr/lib64

--- .github/workflows/install-base.sh
+++ .github/workflows/install-base.sh
@@ -1,18 +1,4 @@
-# ln -s /usr/lib64/libEGL.so /usr/lib
-# ln -s /usr/lib64/libEGL.so.1 /usr/lib
-# ln -s /usr/lib64/libEGL.so.1.0.0 /usr/lib
-# ln -s /usr/lib64/libGL.so /usr/lib
-# ln -s /usr/lib64/libGL.so.1 /usr/lib
-# ln -s /usr/lib64/libGL.so.1.2.0 /usr/lib
-# ln -s /usr/lib64/libGLU.so /usr/lib
-# ln -s /usr/lib64/libGLU.so.1 /usr/lib
-# ln -s /usr/lib64/libGLU.so.1.3.1 /usr/lib
-
-# ls /usr/include
-# ls /usr/lib
-# ls /usr/lib64
-
-ls /usr/lib64/libOpen*.so
+ls /usr/lib64
 
 # # Install OpenCV
 # git clone https://github.com/opencv/opencv.git
