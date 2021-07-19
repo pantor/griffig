7877255ae55930f07bb31fd7ae1f96f39c1c7e04
Lars Berscheid <lars.berscheid@kit.edu>
2021-07-19 14:02:58 +0200

    1

--- .github/workflows/install-base.sh
+++ .github/workflows/install-base.sh
@@ -1,31 +1,33 @@
 ln -s /usr/lib64/libOpenGL.so.0 /usr/lib64/libOpenGL.so
+ls /usr/lib/
+ls /usr/lib64/
 
-# Install Eigen
-git clone https://gitlab.com/libeigen/eigen.git
-cd eigen
-git checkout 3.3.9
-mkdir build && cd build
-cmake ..
-make -j2
-make install
-cd ../../
+# # Install Eigen
+# git clone https://gitlab.com/libeigen/eigen.git
+# cd eigen
+# git checkout 3.3.9
+# mkdir build && cd build
+# cmake ..
+# make -j2
+# make install
+# cd ../../
 
-# Install OpenCV
-git clone https://github.com/opencv/opencv.git
-cd opencv
-git checkout 4.5.2
-mkdir build && cd build
-cmake -DWITH_VTK=OFF -DWITH_GTK=OFF -DWITH_PROTOBUF=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DWITH_WEBP=OFF ..
-make -j2
-make install
-cd ../../
+# # Install OpenCV
+# git clone https://github.com/opencv/opencv.git
+# cd opencv
+# git checkout 4.5.2
+# mkdir build && cd build
+# cmake -DWITH_VTK=OFF -DWITH_GTK=OFF -DWITH_PROTOBUF=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DWITH_WEBP=OFF ..
+# make -j2
+# make install
+# cd ../../
 
-# Install PyBind11
-git clone https://github.com/pybind/pybind11.git
-cd pybind11
-git checkout v2.6.2
-mkdir build && cd build
-cmake -DPYBIND11_TEST=OFF ..
-make -j2
-make install
-cd ../../
+# # Install PyBind11
+# git clone https://github.com/pybind/pybind11.git
+# cd pybind11
+# git checkout v2.6.2
+# mkdir build && cd build
+# cmake -DPYBIND11_TEST=OFF ..
+# make -j2
+# make install
+# cd ../../
