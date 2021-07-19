7d4a2574ac82a2dd0b817ec9880cdf16245aedae
Lars Berscheid <lars.berscheid@kit.edu>
2021-07-19 17:45:16 +0200

    fix

--- .github/workflows/install-base.sh
+++ .github/workflows/install-base.sh
@@ -1,18 +1,11 @@
 ln -s /usr/lib64/libOpenGL.so.0 /usr/lib64/libOpenGL.so
-# ls /usr/lib/
-# ls /usr/lib64/
-# /opt/python/cp36-cp36m/bin/pip install --no-cache-dir numpy
-# /opt/python/cp39-cp39m/bin/pip install --no-cache-dir numpy
 
-python3.6 -m pip install --no-cache-dir numpy
-python3.9 -m pip install --no-cache-dir numpy
-python3.10 -m pip install --no-cache-dir numpy
+# python3.6 -m pip install --no-cache-dir numpy
+# python3.9 -m pip install --no-cache-dir numpy
+# python3.10 -m pip install --no-cache-dir numpy
 sed -i '3092s/.*/"import sys; import numpy; sys.stdout.write(numpy.get_include())"/' /opt/_internal/tools/lib/python3.9/site-packages/cmake/data/share/cmake-3.20/Modules/FindPython/Support.cmake
 
-# python3.10 -c "import sys\ntry: import numpy; sys.stdout.write(numpy.get_include())\nexcept:pass\n"
 
-# /opt/python/cp36-cp36m/bin/pip show numpy
-# ls /usr/local/lib/
 
 # # Install Eigen
 # git clone https://gitlab.com/libeigen/eigen.git
