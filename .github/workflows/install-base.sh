3c735268aa6d42f2b7c3c00ef004c94c0c0e147c
Lars Berscheid <lars.berscheid@kit.edu>
2021-07-19 22:40:30 +0200

    speed up

--- .github/workflows/install-base.sh
+++ .github/workflows/install-base.sh
@@ -4,7 +4,7 @@ export LD_LIBRARY_PATH=/usr/local/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
 
 # Fix CMake find Numpy
 python3.9 -m pip install --no-cache-dir numpy
-python3.10 -m pip install --no-cache-dir numpy
+# python3.10 -m pip install --no-cache-dir numpy
 sed -i '3092s/.*/"import sys; import numpy; sys.stdout.write(numpy.get_include())"/' /opt/_internal/tools/lib/python3.9/site-packages/cmake/data/share/cmake-3.21/Modules/FindPython/Support.cmake
 
 # Install Eigen
@@ -32,7 +32,12 @@ git clone https://github.com/opencv/opencv.git
 cd opencv
 git checkout 4.5.2
 mkdir build && cd build
-cmake -DWITH_VTK=OFF -DWITH_GTK=OFF -DWITH_PROTOBUF=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DWITH_WEBP=OFF -DBUILD_opencv_ml=OFF -DBUILD_opencv_calib3d=OFF -DBUILD_opencv_videoio=OFF -DBUILD_opencv_gapi=OFF -DBUILD_opencv_stitching=OFF -DBUILD_opencv_objdetect=OFF ..
+cmake -DWITH_VTK=OFF -DWITH_GTK=OFF -DWITH_PROTOBUF=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DWITH_WEBP=OFF -DBUILD_opencv_ml=OFF -DBUILD_opencv_calib3d=OFF -DBUILD_opencv_videoio=OFF -DBUILD_opencv_gapi=OFF -DBUILD_opencv_stitching=OFF -DBUILD_opencv_objdetect=OFF -DBUILD_opencv_flann=OFF -DBUILD_opencv_video=OFF -DBUILD_opencv_features2d=OFF ..
 make -j2
 make install
 cd ../../
+
+# ls /usr/lib
+# ls /usr/lib64
+ls /usr/local/lib
+# ls /usr/local/lib64
