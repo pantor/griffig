24c8d8a48a79ad7fc59fe2b1369aaac6fe7d2dd1
Lars Berscheid <lars.berscheid@kit.edu>
2021-07-20 10:36:11 +0200

    ls

--- .github/workflows/install-base.sh
+++ .github/workflows/install-base.sh
@@ -21,12 +21,14 @@ cd pybind11
 git checkout v2.6.2
 cd ../
 
-# Install OpenCV
-git clone https://github.com/opencv/opencv.git
-cd opencv
-git checkout 4.5.2
-mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DWITH_VTK=OFF -DWITH_GTK=OFF -DWITH_PROTOBUF=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DWITH_WEBP=OFF -DBUILD_opencv_ml=OFF -DBUILD_opencv_calib3d=OFF -DBUILD_opencv_videoio=OFF -DBUILD_opencv_gapi=OFF -DBUILD_opencv_stitching=OFF -DBUILD_opencv_objdetect=OFF -DBUILD_opencv_flann=OFF -DBUILD_opencv_video=OFF -DBUILD_opencv_features2d=OFF ..
-make -j2
-make install
-cd ../../
+# # Install OpenCV
+# git clone https://github.com/opencv/opencv.git
+# cd opencv
+# git checkout 4.5.2
+# mkdir build && cd build
+# cmake -DCMAKE_INSTALL_PREFIX=/usr -DWITH_VTK=OFF -DWITH_GTK=OFF -DWITH_PROTOBUF=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DWITH_WEBP=OFF -DBUILD_opencv_ml=OFF -DBUILD_opencv_calib3d=OFF -DBUILD_opencv_videoio=OFF -DBUILD_opencv_gapi=OFF -DBUILD_opencv_stitching=OFF -DBUILD_opencv_objdetect=OFF -DBUILD_opencv_flann=OFF -DBUILD_opencv_video=OFF -DBUILD_opencv_features2d=OFF ..
+# make -j2
+# make install
+# cd ../../
+
+ls
